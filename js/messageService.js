59855b80e661eadd0a49129b373b8511873a148d
Mackenzie <clarkmackenzieb@gmail.com>
2017-09-06 15:20:55 -0500

    completed chat room

--- js/messageService.js
+++ js/messageService.js
@@ -1,14 +1,38 @@
 angular.module('chatroom').service('messageService', function($http){
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
+  var baseurl = 'https://practiceapi.devmountain.com/api/chats';
+
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmountain.com/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
+this.getMessages = function(){
 
+  var promise = $http({
+    method: "GET",
+    url: baseurl,
+  });
+
+  return promise;
+};
 
 
   //On the line below create the postMessage method. This method will add data to the backend server.
+
+  this.postMessage = function(message){
+
+    return $http.post(baseurl, {message: message});
+
+    // var promise = $http({
+    //   method: "POST",
+    //   url: baseurl,
+    //   message: message
+    // });
+
+    // return promise;
+  }
+
   //The url for the request needs to be 'https://practiceapi.devmountain.com/api/chats'
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
